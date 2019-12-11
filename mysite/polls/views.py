582751fce6afbd64e1029929401cf050d32b25d8
Marcos-Pacheco <marcos.hr.pacheco@gmail.com>
2019-12-11 09:18:10 -0300

    Adicionados shortcuts

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Question
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html',{'question':question})
 
 def results(resquest, question_id):
     response = "You're looking at tge results of question %s."
